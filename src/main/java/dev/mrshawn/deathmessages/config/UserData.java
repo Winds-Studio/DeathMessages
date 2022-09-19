2aa626b4a7bfec2c1ae6e9c37f2e459328ef3b8c
LinoxGH <denizcanaksu121@gmail.com>
2022-09-19 15:45:56 +0300

    Saving and loading is now sync when plugin is not enabled.

--- src/main/java/dev/mrshawn/deathmessages/config/UserData.java
+++ src/main/java/dev/mrshawn/deathmessages/config/UserData.java
@@ -34,44 +34,58 @@ public class UserData {
 	private IOOperation ioRunning = null;
 	private final Stack<IOOperation> ioOperations = new Stack<>();
 
-	public void save() {
+	public void save() {this.save(false);}
+	public void save(boolean sync) {
 		if (ioRunning != null) ioOperations.push(IOOperation.SAVE);
 		// We should not halt the main server thread
-		Bukkit.getScheduler().runTaskAsynchronously(DeathMessages.getInstance(), () -> {
-			try {
-				ioRunning = IOOperation.SAVE;
-				ioOperations.removeIf(op -> op == IOOperation.SAVE);
-				config.save(file);
-				ioRunning = null;
-			} catch (IOException e) {
-				File f = new File(DeathMessages.getInstance().getDataFolder(), fileName + ".broken." + new Date().getTime());
-				DeathMessages.getInstance().getLogger().severe("Could not save: " + fileName + ".yml");
-				DeathMessages.getInstance().getLogger().severe("Regenerating file and renaming the current file to: " + f.getName());
-				DeathMessages.getInstance().getLogger().severe("You can try fixing the file with a yaml parser online!");
-				file.renameTo(f);
-				initialize();
-			}
-		});
+		if (!sync) {
+			Bukkit.getScheduler().runTaskAsynchronously(DeathMessages.getInstance(), this::saveFile);
+		} else {
+			saveFile();
+		}
 	}
 
-	public void reload() {
+	private void saveFile() {
+		try {
+			ioRunning = IOOperation.SAVE;
+			ioOperations.removeIf(op -> op == IOOperation.SAVE);
+			config.save(file);
+			ioRunning = null;
+		} catch (IOException e) {
+			File f = new File(DeathMessages.getInstance().getDataFolder(), fileName + ".broken." + new Date().getTime());
+			DeathMessages.getInstance().getLogger().severe("Could not save: " + fileName + ".yml");
+			DeathMessages.getInstance().getLogger().severe("Regenerating file and renaming the current file to: " + f.getName());
+			DeathMessages.getInstance().getLogger().severe("You can try fixing the file with a yaml parser online!");
+			file.renameTo(f);
+			initialize();
+		}
+	}
+
+	public void reload() {this.reload(false);}
+	public void reload(boolean sync) {
 		if (ioRunning != null) ioOperations.push(IOOperation.LOAD);
 		// We should not halt the main server thread
-		Bukkit.getScheduler().runTaskAsynchronously(DeathMessages.getInstance(), () -> {
-			try {
-				ioRunning = IOOperation.LOAD;
-				ioOperations.removeIf(op -> op == IOOperation.LOAD);
-				config.load(file);
-				ioRunning = null;
-			} catch (Exception e) {
-				File f = new File(DeathMessages.getInstance().getDataFolder(), fileName + ".broken." + new Date().getTime());
-				DeathMessages.getInstance().getLogger().severe("Could not reload: " + fileName + ".yml");
-				DeathMessages.getInstance().getLogger().severe("Regenerating file and renaming the current file to: " + f.getName());
-				DeathMessages.getInstance().getLogger().severe("You can try fixing the file with a yaml parser online!");
-				file.renameTo(f);
-				initialize();
-			}
-		});
+		if (!sync) {
+			Bukkit.getScheduler().runTaskAsynchronously(DeathMessages.getInstance(), this::reloadFile);
+		} else {
+			saveFile();
+		}
+	}
+
+	private void reloadFile() {
+		try {
+			ioRunning = IOOperation.LOAD;
+			ioOperations.removeIf(op -> op == IOOperation.LOAD);
+			config.load(file);
+			ioRunning = null;
+		} catch (Exception e) {
+			File f = new File(DeathMessages.getInstance().getDataFolder(), fileName + ".broken." + new Date().getTime());
+			DeathMessages.getInstance().getLogger().severe("Could not reload: " + fileName + ".yml");
+			DeathMessages.getInstance().getLogger().severe("Regenerating file and renaming the current file to: " + f.getName());
+			DeathMessages.getInstance().getLogger().severe("You can try fixing the file with a yaml parser online!");
+			file.renameTo(f);
+			initialize();
+		}
 	}
 
 	public void initialize() {
@@ -86,8 +100,8 @@ public class UserData {
 			}
 		}
 		config = YamlConfiguration.loadConfiguration(file);
-		save();
-		reload();
+		save(true);
+		reload(true);
 	}
 
 	private enum IOOperation {
