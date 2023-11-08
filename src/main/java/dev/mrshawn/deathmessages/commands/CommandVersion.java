c839e2ede17e56f0a93bf3cc4eedec1721436d9a
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2023-11-08 05:29:51 -0500

    Refactor Commands
    - Remove acf commands (useless)
    - Refactor /dm version command
    - Full command TabComplete support

--- src/main/java/dev/mrshawn/deathmessages/commands/CommandVersion.java
+++ src/main/java/dev/mrshawn/deathmessages/commands/CommandVersion.java
@@ -3,6 +3,8 @@ package dev.mrshawn.deathmessages.commands;
 import dev.mrshawn.deathmessages.DeathMessages;
 import dev.mrshawn.deathmessages.enums.Permission;
 import dev.mrshawn.deathmessages.utils.Assets;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextReplacementConfig;
 import org.bukkit.command.CommandSender;
 
 public class CommandVersion extends DeathMessagesCommand {
@@ -18,10 +20,17 @@ public class CommandVersion extends DeathMessagesCommand {
 			DeathMessages.getInstance().adventure().sender(sender).sendMessage(Assets.convertFromLegacy(Assets.formatMessage("Commands.DeathMessages.No-Permission")));
 			return;
 		}
-		String message = Assets.formatMessage("Commands.DeathMessages.Sub-Commands.Version");
-		message = message
-				.replaceAll("%version%", DeathMessages.getInstance().getDescription().getVersion())
-				.replaceAll("%authors%", DeathMessages.getInstance().getDescription().getAuthors().toString());
-		DeathMessages.getInstance().adventure().sender(sender).sendMessage(Assets.convertFromLegacy(message));
+
+		Component message = Assets.convertFromLegacy(Assets.formatMessage("Commands.DeathMessages.Sub-Commands.Version"))
+				.replaceText(TextReplacementConfig.builder()
+						.match("%version%")
+						.replacement(DeathMessages.getInstance().getDescription().getVersion())
+						.build())
+				.replaceText(TextReplacementConfig.builder()
+						.match("%authors%")
+						.replacement(DeathMessages.getInstance().getDescription().getAuthors().toString())
+						.build());
+
+		DeathMessages.getInstance().adventure().sender(sender).sendMessage(message);
 	}
 }
