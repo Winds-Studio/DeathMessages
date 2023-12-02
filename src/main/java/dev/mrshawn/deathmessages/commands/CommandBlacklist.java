cc92ebfe4900f3b1472e1a12df07036f6e87ec1e
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2023-12-02 18:27:40 -0500

    Add support for Blacklist command when Saved-User-Data disabled

--- src/main/java/dev/mrshawn/deathmessages/commands/CommandBlacklist.java
+++ src/main/java/dev/mrshawn/deathmessages/commands/CommandBlacklist.java
@@ -2,8 +2,10 @@ package dev.mrshawn.deathmessages.commands;
 
 import dev.mrshawn.deathmessages.DeathMessages;
 import dev.mrshawn.deathmessages.api.PlayerManager;
+import dev.mrshawn.deathmessages.config.Settings;
 import dev.mrshawn.deathmessages.config.UserData;
 import dev.mrshawn.deathmessages.enums.Permission;
+import dev.mrshawn.deathmessages.files.Config;
 import dev.mrshawn.deathmessages.utils.Assets;
 import net.kyori.adventure.text.TextReplacementConfig;
 import org.bukkit.Bukkit;
@@ -34,35 +36,51 @@ public class CommandBlacklist extends DeathMessagesCommand {
 					.replacement(args[0])
 					.build();
 
-			for (Map.Entry<String, Object> entry : UserData.getInstance().getConfig().getValues(false).entrySet()) {
-				String username = UserData.getInstance().getConfig().getString(entry.getKey() + ".username");
-				if (username.equalsIgnoreCase(args[0])) {
-					boolean blacklisted = UserData.getInstance().getConfig().getBoolean(entry.getKey() + ".is-blacklisted");
-					if (blacklisted) {
-						if (Bukkit.getPlayer(UUID.fromString(entry.getKey())) != null) {
-							Optional<PlayerManager> getPlayer = PlayerManager.getPlayer(UUID.fromString(entry.getKey()));
-							getPlayer.ifPresent(pm -> pm.setBlacklisted(false));
-						}
-						UserData.getInstance().getConfig().set(entry.getKey() + ".is-blacklisted", false);
-						UserData.getInstance().save();
-						DeathMessages.getInstance().adventure().sender(sender).sendMessage(Assets.convertFromLegacy(Assets.formatMessage("Commands.DeathMessages.Sub-Commands.Blacklist.Blacklist-Remove"))
-								.replaceText(player));
-					} else {
-						if (Bukkit.getPlayer(UUID.fromString(entry.getKey())) != null) {
-							Optional<PlayerManager> getPlayer = PlayerManager.getPlayer(UUID.fromString(entry.getKey()));
-							getPlayer.ifPresent(pm -> pm.setBlacklisted(true));
-						}
-						UserData.getInstance().getConfig().set(entry.getKey() + ".is-blacklisted", true);
-						UserData.getInstance().save();
-						DeathMessages.getInstance().adventure().sender(sender).sendMessage(Assets.convertFromLegacy(Assets.formatMessage("Commands.DeathMessages.Sub-Commands.Blacklist.Blacklist-Add"))
-								.replaceText(player));
-					}
-					return;
-				}
+			// Saved-User-Data disabled
+			if (!Settings.getInstance().getConfig().getBoolean(Config.SAVED_USER_DATA.getPath())) {
+				Optional<PlayerManager> getPlayer = PlayerManager.getPlayer(Bukkit.getPlayer(args[0]));
+				getPlayer.ifPresentOrElse(pm -> {
+							if (pm.isBlacklisted()) {
+								pm.setBlacklisted(false);
+								DeathMessages.getInstance().adventure().sender(sender).sendMessage(Assets.convertFromLegacy(Assets.formatMessage("Commands.DeathMessages.Sub-Commands.Blacklist.Blacklist-Remove"))
+										.replaceText(player));
+							} else {
+								pm.setBlacklisted(true);
+								DeathMessages.getInstance().adventure().sender(sender).sendMessage(Assets.convertFromLegacy(Assets.formatMessage("Commands.DeathMessages.Sub-Commands.Blacklist.Blacklist-Add"))
+										.replaceText(player));
+							}
+						}, () -> DeathMessages.getInstance().adventure().sender(sender).sendMessage(Assets.convertFromLegacy(Assets.formatMessage("Commands.DeathMessages.Sub-Commands.Blacklist.Username-None-Existent"))
+								.replaceText(player))
+				);
+				return;
 			}
-			DeathMessages.getInstance().adventure().sender(sender).sendMessage(Assets.convertFromLegacy(Assets.formatMessage("Commands.DeathMessages.Sub-Commands.Blacklist.Username-None-Existent"))
-					.replaceText(player));
-		}
 
+			// Saved-User-Data enabled
+			for (Map.Entry<String, Object> entry : UserData.getInstance().getConfig().getValues(false).entrySet()) {
+				Optional<PlayerManager> getPlayer = PlayerManager.getPlayer(UUID.fromString(entry.getKey()));
+				getPlayer.ifPresentOrElse(pm -> {
+					String username = UserData.getInstance().getConfig().getString(entry.getKey() + ".username");
+					if (username.equalsIgnoreCase(args[0])) {
+						boolean blacklisted = UserData.getInstance().getConfig().getBoolean(entry.getKey() + ".is-blacklisted");
+						if (blacklisted) {
+							pm.setBlacklisted(false);
+							UserData.getInstance().getConfig().set(entry.getKey() + ".is-blacklisted", false);
+							UserData.getInstance().save();
+							DeathMessages.getInstance().adventure().sender(sender).sendMessage(Assets.convertFromLegacy(Assets.formatMessage("Commands.DeathMessages.Sub-Commands.Blacklist.Blacklist-Remove"))
+									.replaceText(player));
+						} else {
+							pm.setBlacklisted(true);
+							UserData.getInstance().getConfig().set(entry.getKey() + ".is-blacklisted", true);
+							UserData.getInstance().save();
+							DeathMessages.getInstance().adventure().sender(sender).sendMessage(Assets.convertFromLegacy(Assets.formatMessage("Commands.DeathMessages.Sub-Commands.Blacklist.Blacklist-Add"))
+									.replaceText(player));
+						}
+
+					}
+				}, () -> DeathMessages.getInstance().adventure().sender(sender).sendMessage(Assets.convertFromLegacy(Assets.formatMessage("Commands.DeathMessages.Sub-Commands.Blacklist.Username-None-Existent"))
+						.replaceText(player)));
+				return;
+			}
+		}
 	}
 }
