1af7c9b4f596d654c4c50ecc70068ebb1eea8cd9
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2023-12-04 09:44:57 -0500

    Add missing message nodes

--- src/main/java/dev/mrshawn/deathmessages/utils/Assets.java
+++ src/main/java/dev/mrshawn/deathmessages/utils/Assets.java
@@ -554,6 +554,7 @@ public class Assets {
 		if (msgs.isEmpty()) {
 			msgs = sortList(getPlayerDeathMessages().getStringList(DeathModes.MOBS.getValue() + ".player." + affiliation + "." + damageCause), pm.getPlayer(), mob);
 			if (msgs.isEmpty()) {
+				LogManager.getLogger(DeathMessages.getInstance().getName()).info("Redirected from [" + DeathModes.MOBS.getValue() + ".player." + affiliation + "." + damageCause + "]");
 				if (Settings.getInstance().getConfig().getBoolean(Config.DEFAULT_NATURAL_DEATH_NOT_DEFINED.getPath()))
 					return getNaturalDeath(pm, damageCause);
 				if (Settings.getInstance().getConfig().getBoolean(Config.DEFAULT_MELEE_LAST_DAMAGE_NOT_DEFINED.getPath()))
@@ -604,6 +605,7 @@ public class Assets {
 		if (msgs.isEmpty()) {
 			msgs = sortList(getPlayerDeathMessages().getStringList(DeathModes.MOBS.getValue() + ".player." + affiliation + "." + projectileDamage), pm.getPlayer(), mob);
 			if (msgs.isEmpty()) {
+				LogManager.getLogger(DeathMessages.getInstance().getName()).info("Redirected from [" + DeathModes.MOBS.getValue() + ".player." + affiliation + "." + projectileDamage + "]");
 				if (Settings.getInstance().getConfig().getBoolean(Config.DEFAULT_NATURAL_DEATH_NOT_DEFINED.getPath()))
 					return getNaturalDeath(pm, projectileDamage);
 				LogManager.getLogger(DeathMessages.getInstance().getName()).warn("This death message will not be broadcast, unless you enable [Default-Natural-Death-Not-Defined] in Settings.yml");
