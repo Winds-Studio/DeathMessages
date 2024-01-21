e1d865140fb81705e670d50e6748cc811a88f550
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2024-01-21 16:38:00 -0500

    Improve TabComplete code readability

--- Core/src/main/java/dev/mrshawn/deathmessages/commands/TabCompleter.java
+++ Core/src/main/java/dev/mrshawn/deathmessages/commands/TabCompleter.java
@@ -7,36 +7,37 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class TabCompleter implements org.bukkit.command.TabCompleter {
 
-	List<String> arguments = new ArrayList<>();
+	List<String> arguments;
 
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
 		if (args.length == 1) {
-			arguments.clear();
-			arguments.add("backup");
-			arguments.add("blacklist");
-			arguments.add("discordlog");
-			arguments.add("edit");
-			arguments.add("reload");
-			arguments.add("restore");
-			arguments.add("toggle");
-			arguments.add("version");
+			arguments = Arrays.asList(
+					"backup",
+					"blacklist",
+					"discordlog",
+					"edit",
+					"reload",
+					"restore",
+					"toggle",
+					"version"
+			);
 			// Dreeam - refer to https://github.com/mrgeneralq/sleep-most/blob/5f2f7772c9715cf57530e2af3573652d17cd7420/src/main/java/me/mrgeneralq/sleepmost/commands/SleepmostCommand.java#L135
 			return arguments.stream()
 					.filter(arg -> sender.hasPermission("deathmessages.command." + arg))
 					.collect(Collectors.toList());
 		} else if (args.length == 2) {
 			if (args[0].equalsIgnoreCase("backup")) {
-				arguments.clear();
-				arguments.add("true");
-				arguments.add("false");
-				return arguments;
+				return Arrays.asList(
+						"true",
+						"false"
+				);
 			} else if (args[0].equalsIgnoreCase("blacklist")) {
 				arguments.clear();
 				for (Player player : Bukkit.getServer().getOnlinePlayers()) {
@@ -44,16 +45,16 @@ public class TabCompleter implements org.bukkit.command.TabCompleter {
 				}
 				return arguments;
 			} else if (args[0].equalsIgnoreCase("edit")) {
-				arguments.clear();
-				arguments.add("player");
-				arguments.add("entity");
-				return arguments;
+				return Arrays.asList(
+						"player",
+						"entity"
+				);
 			}
 		} else if (args.length == 3 && args[0].equalsIgnoreCase("restore")) {
-			arguments.clear();
-			arguments.add("true");
-			arguments.add("false");
-			return arguments;
+			return Arrays.asList(
+					"true",
+					"false"
+			);
 		}
 
 		// Dreeam TODO - Command edit Will be removed since DeathMessages 1.4.19
@@ -75,10 +76,10 @@ public class TabCompleter implements org.bukkit.command.TabCompleter {
 			} else if (args.length == 4) {
 				// /dm edit <player> <mobName> <solo, gang>
 				if (args[1].equalsIgnoreCase("player")) {
-					arguments.clear();
-					arguments.add("solo");
-					arguments.add("gang");
-					return arguments;
+					return Arrays.asList(
+							"solo",
+							"gang"
+					);
 				} else if (args[1].equalsIgnoreCase("entity")) {
 					return DamageTypes.getFriendlyNames();
 				}
@@ -87,20 +88,20 @@ public class TabCompleter implements org.bukkit.command.TabCompleter {
 				if (args[1].equalsIgnoreCase("player")) {
 					return DamageTypes.getFriendlyNames();
 				} else if (args[1].equalsIgnoreCase("entity")) {
-					arguments.clear();
-					arguments.add("add");
-					arguments.add("remove");
-					arguments.add("list");
-					return arguments;
+					return Arrays.asList(
+							"add",
+							"remove",
+							"list"
+					);
 				}
 			} else if (args.length == 6) {
 				// /dm edit <player> <mobName> <solo, gang> <damage-type> <add, remove, list>
 				if (args[1].equalsIgnoreCase("player")) {
-					arguments.clear();
-					arguments.add("add");
-					arguments.add("remove");
-					arguments.add("list");
-					return arguments;
+					return Arrays.asList(
+							"add",
+							"remove",
+							"list"
+					);
 				}
 			}
 		}
