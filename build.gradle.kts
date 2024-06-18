e35c20f513b47337a057bb2ca916958073a1c679
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2024-06-19 06:20:44 +0800

    Fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,11 @@ allprojects {
             url = uri("https://repo.auxilor.io/repository/maven-public/")
         }
     }
+
+    configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
 // Dreeam end
 
