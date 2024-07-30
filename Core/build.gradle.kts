79063a65f802c35aa502367fc20a074c72d60179
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2024-07-31 01:29:29 +0800

    Bump dependencies

--- Core/build.gradle.kts
+++ Core/build.gradle.kts
@@ -15,19 +15,19 @@ dependencies {
     implementation(project(":NMS:V1_21"))
 
     compileOnly("org.spigotmc:spigot-api:1.21-R0.1-SNAPSHOT") // Universal
-    compileOnly("org.apache.commons:commons-lang3:3.14.0")
+    compileOnly("org.apache.commons:commons-lang3:3.15.0")
     compileOnly("commons-io:commons-io:2.16.1")
     compileOnly("org.apache.logging.log4j:log4j-api:2.23.1")
     implementation("com.github.cryptomorin:XSeries:11.2.0.1")
     implementation("org.bstats:bstats-bukkit:3.0.2")
     implementation("com.tcoded:FoliaLib:0.4.0")
 
-    implementation("de.tr7zw:item-nbt-api:2.13.1-SNAPSHOT")
+    implementation("de.tr7zw:item-nbt-api:2.13.2-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.6")
     compileOnly("com.discordsrv:discordsrv:1.27.0")
     compileOnly("io.lumine:Mythic-Dist:5.6.2")
-    compileOnly("com.willfp:eco:6.71.6")
-    compileOnly("com.willfp:EcoEnchants:12.12.1")
+    compileOnly("com.willfp:eco:6.73.1")
+    compileOnly("com.willfp:EcoEnchants:12.17.0")
     compileOnly("com.sk89q.worldguard:worldguard-legacy:6.2")
     compileOnly("com.github.sirblobman.combatlogx:api:11.4-SNAPSHOT")
     compileOnly("com.meowj:LangUtils:1.9")
