c6b0b5dac47dfb7572d9baf5373daaff88204527
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2024-09-08 03:14:58 -0400

    Optimize and better TabCompletion

--- Core/src/main/java/dev/mrshawn/deathmessages/commands/TabCompleter.java
+++ Core/src/main/java/dev/mrshawn/deathmessages/commands/TabCompleter.java
@@ -8,27 +8,33 @@ import org.bukkit.entity.Player;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class TabCompleter implements org.bukkit.command.TabCompleter {
 
-	List<String> arguments = new ArrayList<>();
+	private final List<String> tabCompletion = new ArrayList<>(Arrays.asList(
+			"backup",
+			"blacklist",
+			"discordlog",
+			"reload",
+			"restore",
+			"toggle",
+			"version"
+	));
 
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
 		if (args.length == 1) {
+			List<String> completions = new ArrayList<>();
+
 			// Dreeam - refer to https://github.com/mrgeneralq/sleep-most/blob/5f2f7772c9715cf57530e2af3573652d17cd7420/src/main/java/me/mrgeneralq/sleepmost/commands/SleepmostCommand.java#L135
-			return Stream.of(
-							"backup",
-							"blacklist",
-							"discordlog",
-							"reload",
-							"restore",
-							"toggle",
-							"version"
-					).filter(arg -> sender.hasPermission("deathmessages.command." + arg))
-					.collect(Collectors.toList());
+			for (String completion: tabCompletion) {
+				final String arg = args[0];
+				if (completion.startsWith(arg) && sender.hasPermission("deathmessages.command." + arg)) {
+					completions.add(completion);
+				}
+			}
+
+			return completions;
 		} else if (args.length == 2) {
 			if (args[0].equalsIgnoreCase("backup")) {
 				return Arrays.asList(
@@ -36,11 +42,15 @@ public class TabCompleter implements org.bukkit.command.TabCompleter {
 						"false"
 				);
 			} else if (args[0].equalsIgnoreCase("blacklist")) {
-				arguments.clear();
+				final List<String> players = new ArrayList<>();
+
 				for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-					arguments.add(player.getName());
+					if (player.getName().toLowerCase().startsWith(args[1])) {
+						players.add(player.getName());
+					}
 				}
-				return arguments;
+
+				return players;
 			} else if (args[0].equalsIgnoreCase("edit")) {
 				return Arrays.asList(
 						"player",
