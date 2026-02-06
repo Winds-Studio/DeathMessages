f8cde3f7d1b50e98693a4af918609d5bc8eaae5b
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2026-02-06 02:01:37 -0500

    Disable self if the user uses the wrong jar

--- DeathMessagesLegacy/src/main/java/dev/mrshawn/deathmessages/utils/PlatformUtil.java
+++ DeathMessagesLegacy/src/main/java/dev/mrshawn/deathmessages/utils/PlatformUtil.java
@@ -1,12 +1,16 @@
 package dev.mrshawn.deathmessages.utils;
 
+import dev.mrshawn.deathmessages.DeathMessages;
+import org.bukkit.Bukkit;
+
 public class PlatformUtil {
 
     //public static final boolean IS_FOLIA = DeathMessages.getInstance().foliaLib.isFolia();
 
     public static void init() {
         if (Util.isNewerAndEqual(20, 5)) {
-            throw new UnsupportedOperationException("You should use DeathMessagesModern for version >= 1.20.5");
+            DeathMessages.LOGGER.error("You should use DeathMessagesModern for version >= 1.20.5");
+            Bukkit.getPluginManager().disablePlugin(DeathMessages.getInstance());
         }
     }
 }
--- DeathMessagesModern/src/main/java/dev/mrshawn/deathmessages/utils/PlatformUtil.java
+++ DeathMessagesModern/src/main/java/dev/mrshawn/deathmessages/utils/PlatformUtil.java
@@ -1,12 +1,16 @@
 package dev.mrshawn.deathmessages.utils;
 
+import dev.mrshawn.deathmessages.DeathMessages;
+import org.bukkit.Bukkit;
+
 public class PlatformUtil {
 
     //public static final boolean IS_FOLIA = DeathMessages.getInstance().foliaLib.isFolia();
 
     public static void init() {
         if (Util.isOlderAndEqual(20, 4)) {
-            throw new UnsupportedOperationException("You should use DeathMessagesLegacy for version <= 1.20.4");
+            DeathMessages.LOGGER.error("You should use DeathMessagesLegacy for version <= 1.20.4");
+            Bukkit.getPluginManager().disablePlugin(DeathMessages.getInstance());
         }
     }
 }
